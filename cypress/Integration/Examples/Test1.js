60c06301f5f80a8d025a868372410949020ea6a4
pranav1784 <pranavgambhir165@gmail.com>
2023-10-07 18:26:01 +0530

    Modified the comment

--- cypress/Integration/Examples/Test1.js
+++ cypress/Integration/Examples/Test1.js
@@ -17,6 +17,7 @@ describe("My First Test Suite", function () {
         const textVeg = $el.find("h4.product-name").text();
         if (textVeg.includes("Cashews")) {
           cy.wrap($el).find("button").click();
+          //comments
         }
       });
   });
