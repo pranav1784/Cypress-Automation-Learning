7ca17bfcec1ae01bb42e50abc180753338cb86e3
pranav1784 <pranavgambhir165@gmail.com>
2023-10-07 23:25:23 +0530

    Added Another file

--- cypress/Integration/Examples/Test1.js
+++ cypress/Integration/Examples/Test1.js
@@ -2,42 +2,42 @@
 //Test case is called spec in cypress
 describe("My First Test Suite", function () {
   it("My First Test Case", function () {
-    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-    cy.get(".search-keyword").type("ca");
-    cy.wait(2000);
-    cy.get(".product:visible").should("have.length", 4);
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get(".search-keyword").type("ca")
+    cy.wait(2000)
+    cy.get(".product:visible").should("have.length", 4)
 
     //parent child chaining
-    cy.get(".products").as("ProductsCatalog");
-    cy.get("@ProductsCatalog").find(".product").should("have.length", 4);
+    cy.get(".products").as("ProductsCatalog")
+    cy.get("@ProductsCatalog").find(".product").should("have.length", 4)
     //cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
     cy.get("@ProductsCatalog")
       .find(".product")
       .contains("ADD TO CART")
       .click()
       .then(function () {
-        console.log("Add to cart clicked");
-      });
+        console.log("Add to cart clicked")
+      })
     cy.get("@ProductsCatalog")
       .find(".product")
       .each(($el, index, $list) => {
-        const textVeg = $el.find("h4.product-name").text();
+        const textVeg = $el.find("h4.product-name").text()
         if (textVeg.includes("Cashews")) {
-          cy.wrap($el).find("button").click();
+          cy.wrap($el).find("button").click()
           //comments
           //comment2
         }
-      });
+      })
     //Assert iftext is correctly displayed
-    cy.get(".greenLogo").should("have.text", "GREENKART");
+    cy.get(".greenLogo").should("have.text", "GREENKART")
 
     //Below code is to print the log in cypress
     cy.get(".greenLogo").then(function (logoText) {
-      cy.log(logoText.text());
-    });
+      cy.log(logoText.text())
+    })
     /*cypress supports jquery methods, as it is known that cypress is asynchronous in nature it can identify methods owned by cypress
        but if any method is not written in cypress it cannot resolve promise of that mehod, forr that case promise needs to be resolved manually*/
 
     // cy.log(cy.get(".greenLogo").text());
-  });
-});
+  })
+})
---
+++ cypress/Integration/Examples/Test2.js
@@ -0,0 +1,29 @@
+///  <reference types="Cypress"/>
+//Test case is called spec in cypress
+describe("My Second Test Suite", function () {
+  it("My Second Test Case", function () {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    cy.get(".search-keyword").type("ca")
+    cy.wait(2000)
+
+    //parent child chaining
+    cy.get(".products").as("ProductsCatalog")
+    cy.get("@ProductsCatalog")
+      .find(".product")
+      .contains("ADD TO CART")
+      .click()
+      .then(function () {
+        console.log("Add to cart clicked")
+      })
+    cy.get("@ProductsCatalog")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text()
+        if (textVeg.includes("Cashews")) {
+          cy.wrap($el).find("button").click()
+          //comments
+          //comment2
+        }
+      })
+  })
+})
