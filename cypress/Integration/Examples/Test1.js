aac3f05350f35e8906305a2d0a69a4eefb7dbbce
pranav1784 <pranavgambhir165@gmail.com>
2023-10-07 17:43:07 +0530

    First Commit

---
+++ cypress/Integration/Examples/Test1.js
@@ -0,0 +1,23 @@
+///  <reference types="Cypress"/>
+//Test case is called spec in cypress
+describe("My First Test Suite", function () {
+  it("My First Test Case", function () {
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    cy.get(".product:visible").should("have.length", 4);
+
+    //parent child chaining
+    cy.get(".products").find(".product").should("have.length", 4);
+    //cy.get(".products").find(".product").eq(2).contains("ADD TO CART").click();
+    cy.get(".products").find(".product").contains("ADD TO CART").click();
+    cy.get(".products")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text();
+        if (textVeg.includes("Cashews")) {
+          cy.wrap($el).find("button").click();
+        }
+      });
+  });
+});
