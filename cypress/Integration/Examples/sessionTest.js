16b476a77a04cab14f4ef43f8ca3b8e9c55247fb
pranav1784 <pranavgambhir165@gmail.com>
2023-10-17 17:32:08 +0530

    added some files for neat csv

--- cypress/Integration/Examples/sessionTest.js
+++ cypress/Integration/Examples/sessionTest.js
@@ -1,12 +1,11 @@
-describe('My Session Test Suite',()=>{
-    it('Session Test Case',()=>{
-        cy.LoginFunction().then(()=>{
-            cy.visit('https://rahulshettyacademy.com/client',{
-                onBeforeLoad:function(window){
-                    window.localStorage.setItem('token',Cypress.env('token'))
-                }
-            })
-        })
-
+describe("My Session Test Suite", () => {
+  it("Session Test Case", () => {
+    cy.LoginFunction().then(() => {
+      cy.visit("https://rahulshettyacademy.com/client", {
+        onBeforeLoad: function (window) {
+          window.localStorage.setItem("token", Cypress.env("token"))
+        },
+      })
     })
+  })
 })
