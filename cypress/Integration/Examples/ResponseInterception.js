77c240de67382130a69b037de5ac0e151a1236de
pranav1784 <pranavgambhir165@gmail.com>
2023-10-18 17:56:36 +0530

    minor

--- cypress/Integration/Examples/ResponseInterception.js
+++ cypress/Integration/Examples/ResponseInterception.js
@@ -1,27 +1,31 @@
 ///  <reference types="Cypress"/>
 describe("Response Interception Test Suite", function () {
-it("My Interception case:",function(){
-
+  it("My Interception case:", function () {
     //syntax to mock the response
-    cy.intercept({
-  method:"GET",
-  url:"https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty"      
-    },{
-        statusCode:200,
-        body:[{
-            
-                "book_name": "Physics",
-                "isbn": "mock_code01",
-                "aisle": "test101"
-
-        }]
-    }).as("bookResponseIntercept")
+    cy.intercept(
+      {
+        method: "GET",
+        url: "https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty",
+      },
+      {
+        statusCode: 200,
+        body: [
+          {
+            book_name: "Physics",
+            isbn: "mock_code01",
+            aisle: "test101",
+          },
+        ],
+      }
+    ).as("bookResponseIntercept")
     cy.visit("https://rahulshettyacademy.com/angularAppdemo/")
     cy.get("button.btn-primary").click()
-    cy.wait("@bookResponseIntercept").then(({request,response})=>{
-        cy.get('tr').should('have.length',response.body.length+1)
-
+    cy.wait("@bookResponseIntercept").then(({ request, response }) => {
+      cy.get("tr").should("have.length", response.body.length + 1)
     })
-    cy.get("p").should("contain.text","Oops")
-})
+    cy.get("p").should("contain.text", "Oops")
+  })
+  //   it("My second Intercception Case:", function () {
+  //     cy.visit("https://rahulshettyacademy.com/angularAppdemo/")
+  //   })
 })
